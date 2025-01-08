62b45496a1fd5ca1534b5323b71a47586fee5511
Yves Steinbach <44905871+yvessteinbach@users.noreply.github.com>
2025-01-08 21:06:53 +0100

    s

--- app/page.tsx
+++ app/page.tsx
@@ -32,9 +32,9 @@ const StoryGenerator: React.FC = () => {
 
     if (numberElement) numberElement.innerHTML = `Nr.<br>${number}`;
     if (smallTitleElement) smallTitleElement.textContent = smallTitle;
-    if (desc1Element) desc1Element.textContent = formattedDate;
-    if (desc2Element) desc2Element.textContent = desc2 + ` Uhr`;
-    if (desc3Element) desc3Element.textContent = desc3;
+    if (desc1Element) desc1Element.textContent = `📅 ` + formattedDate;
+    if (desc2Element) desc2Element.textContent = `🕑 ` + desc2 + ` Uhr`;
+    if (desc3Element) desc3Element.textContent = `👨‍🚒 ` + desc3;
   };
 
   const exportAsImage = () => {
