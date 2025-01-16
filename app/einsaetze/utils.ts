d3c39bf89db6b6cdd5e9d83c0e9124898a56af15
Yves Steinbach <44905871+yvessteinbach@users.noreply.github.com>
2025-01-16 17:01:53 +0100

    as

--- app/einsaetze/utils.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs/promises';
-import path from 'path';
-
-export async function readEinsaetze() {
-    const filePath = path.join(process.cwd(), 'app', 'data', 'einsaetze.json');
-    const fileContents = await fs.readFile(filePath, 'utf8');
-    return JSON.parse(fileContents);
-}
