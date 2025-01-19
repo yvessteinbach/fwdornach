c0ee00b8d37a007fd7d73bce378342daff08a188
Yves Steinbach <44905871+yvessteinbach@users.noreply.github.com>
2025-01-19 19:45:48 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,12 +1,21 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyAqWHxph1lIAc1H-Kh-sGVdJ2OWwX19fwQ",
+    authDomain: "fwdornach.firebaseapp.com",
+    projectId: "fwdornach",
+    storageBucket: "fwdornach.firebasestorage.app",
+    messagingSenderId: "673184769493",
+    appId: "1:673184769493:web:8a1aa3130344cd6b78095c",
+    measurementId: "G-R709XVLTVX"
 };
 
-export const firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase app
+const firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+// Initialize Firebase Auth
+export const auth = getAuth(firebaseApp);
+
+export default firebaseApp;
